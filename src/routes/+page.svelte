1f2aac188795f198b592816d70037505bc3fadca
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2024-10-04 05:44:29 +0600

    add

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,65 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div
+	data-main=""
+	class="bg-white rounded-3xl h-full w-full flex justify-center items-center flex-col gap-10"
+>
+	<div class="font-bold text-3xl" style="color:#44B79A">Let's create an account</div>
+	<div class="flex flex-col items-start gap-2">
+		<div>Your name <span style="color:#E45270">*</span></div>
+		<div class="w-full max-w-sm min-w-[442px]">
+			<input
+				class="w-full bg-transparent placeholder:text-[#9C9AA5] text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+				placeholder="Type here..."
+			/>
+		</div>
+	</div>
+
+	<div class="flex flex-col items-start gap-2">
+		<div>Phone Number <span style="color:#E45270">*</span></div>
+		<div class="w-full max-w-sm min-w-[442px]">
+			<input
+				class="w-full bg-transparent placeholder:text-[#9C9AA5] text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+				placeholder="Type here..."
+			/>
+		</div>
+	</div>
+
+	<div class="flex flex-col items-start gap-2">
+		<div>Email</div>
+		<div class="w-full max-w-sm min-w-[442px]">
+			<input
+				class="w-full bg-transparent placeholder:text-[#9C9AA5] text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+				placeholder="Type here..."
+			/>
+		</div>
+	</div>
+
+	<div class="flex flex-col items-start gap-2">
+		<div>Location</div>
+		<div class="w-full max-w-sm min-w-[442px]">
+			<input
+				class="w-full bg-transparent placeholder:text-[#9C9AA5] text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+				placeholder="Type here..."
+			/>
+		</div>
+	</div>
+
+	<div class="flex flex-col items-start gap-2">
+		<div>Language</div>
+		<div class="w-full max-w-sm min-w-[442px]">
+			<input
+				class="w-full bg-transparent placeholder:text-[#9C9AA5] text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+				placeholder="Type here..."
+			/>
+		</div>
+	</div>
+
+	<div>Have an Account? Sign In</div>
+
+	<button class="w-52 h-12" style="background-color:#44B79A">Submit</button>
+</div>
+
+<style>
+	div[data-main] {
+		font-family: 'Nunito';
+	}
+</style>
